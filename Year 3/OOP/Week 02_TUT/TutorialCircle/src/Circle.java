40bfbdb0a822c3ec83e6f9a4f2df92759938b503
Udith Kavishka <udithmanohara@gmail.com>
2024-10-07 23:13:44 +0530

    Final commit

--- Year 3/OOP/Week 02_TUT/TutorialCircle/src/Circle.java
+++ Year 3/OOP/Week 02_TUT/TutorialCircle/src/Circle.java
@@ -1,35 +1,37 @@
 public class Circle {
+
+// Two private instance variables: radius of type double and colour of type String (with a default value of 1  and “blue” respectively)
     private double radius;
     private String color;
 
+// Two overloaded constructors: a default constructor with no argument, and another that takes an argument of type double for the radius
+
     public Circle(){
         this.radius = 1;
-        this.color = "Blue";
+        this.color = "blue";
     }
 
-    public Circle (double r){
-        radius = r;
+    public Circle (double radius){
+        this.radius = radius;
     }
 
-    public Circle (double r, String co){
-        radius = r;
-        color = co;
+    public Circle (String color, double radius){
+        this.color = color;
+        this.radius = radius;
     }
 
-    public void setRadius(int r){
-        radius = r;
-    }
+
+// Two public methods that return the radius and the area respectively, for this instance.
     public double getRadius(){
         return radius;
     }
 
     public double getArea(){
-        return (Math.PI * getRadius()* getRadius());
+        return (Math.PI * radius * radius);
     }
 
-    public String getColor() {
+    public String getColor(){
         return color;
     }
-
 
 }
---
+++ Year 3/OOP/Week 02_TUT/TutorialCircle/src/TutorialCircle.java
@@ -0,0 +1,17 @@
+public class TutorialCircle {
+    public static void main(String[] args) {
+        
+        Circle circleOne = new Circle();
+        System.out.println(circleOne.getRadius());
+        System.out.println(circleOne.getArea());
+
+        Circle circleTwo = new Circle(7);
+        System.out.println(circleTwo.getRadius());
+        System.out.println(circleTwo.getArea());     
+        
+        Circle circleThree = new Circle("purple", 26);
+        System.out.println(circleThree.getRadius());
+        System.out.println(circleThree.getColor());
+        System.out.println(circleThree.getArea());
+    }
+}
