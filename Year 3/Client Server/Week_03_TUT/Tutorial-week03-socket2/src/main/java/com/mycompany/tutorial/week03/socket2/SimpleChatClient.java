46f6190968d2bbf779187606bf8fad6d67693ba4
UKhira <106836713+UKhira@users.noreply.github.com>
2025-03-09 23:05:46 +0530

    chat app

--- Year 3/Client Server/Week_03_TUT/Tutorial-week03-socket2/src/main/java/com/mycompany/tutorial/week03/socket2/SimpleChatClient.java
+++ Year 3/Client Server/Week_03_TUT/Tutorial-week03-socket2/src/main/java/com/mycompany/tutorial/week03/socket2/SimpleChatClient.java
@@ -8,59 +8,74 @@ package com.mycompany.tutorial.week03.socket2;
 /* **Import necessary packages */
 
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
 
 /* **Define a public class named SimpleChatClient. This class will contain all the logic for our chat client program.** */
-
+public class SimpleChatClient {
 
     /* **Define the main method. This is the entry point for any Java application.** */
-    
+    public static void main(String[] args) {
 
-        /* **Try to establish a connection to the server and handle communication with the server. 
+        /* **Try to establish a connection to the server and handle communication with the server.
         If an IOException occurs, it will be caught and handled in the catch block.** */
-       
+        try{
 
-            /* **Create a new Socket object to connect to the server. 
+            /* **Create a new Socket object to connect to the server.
             The Socket constructor takes two parameters: the server's IP address (or hostname) and the port number.** */
-
+            Socket socket = new Socket("localhost",12345);
 
             /* **Print a message to the console indicating that the client has connected to the server.** */
+            System.out.println("Client has been connected to server");
 
             /* **Get the input stream of the socket. This stream is used to receive data from the server.** */
-=
+            InputStream in = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
             /* **Get the output stream of the socket. This stream is used to send data to the server.** */
+            OutputStream out = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(out, true);
 
-
-            /* **Create a Scanner object to read input from the user. The Scanner constructor takes an input stream as a parameter. 
+                 /* **Create a Scanner object to read input from the user. The Scanner constructor takes an input stream as a parameter.
             Here, we pass System.in to read input from the console.** */
-
+            Scanner scanner = new Scanner(System.in);
 
             /* **Enter an infinite loop where the client will continuously send messages to the server and receive responses.** */
-
+            while(true){
 
                 /* **Prompt the user to enter a message.** */
-
+                System.out.println("Enter message: ");
 
                 /* **Read a line of input from the user.** */
+                String message = scanner.nextLine();
 
-
-                /* **Send the user's message to the server. The write method of the OutputStream class is used here. 
+                  /* **Send the user's message to the server. The write method of the OutputStream class is used here.
                 It takes a byte array as a parameter, so we call the getBytes method of the String class to convert the message to bytes.** */
+                writer.println(message);
 
 
                 /* **Create a byte array to store the server's response. you may name it "buffer"** */
+                byte[] buffer = new byte[1024];
 
-
-                /* **Read the server's response into the buffer. The read method of the InputStream class is used here. 
+                 /* **Read the server's response into the buffer. The read method of the InputStream class is used here.
                 It reads data from the input stream into the buffer and returns the number of bytes read.** */
+                int bytesRead = in.read(buffer);
 
 
-                /* **Convert the server's response to a string. The String constructor is used here. 
+                /* **Convert the server's response to a string. The String constructor is used here.
                 It takes three parameters: the byte array containing the data, the offset to start at (0 in this case), and the number of bytes to convert.** */
-
+                String serverResponse = new String(buffer, 0, bytesRead);
 
                 /* **Print the server's response to the console.** */
-            
+                System.out.println("Server: " + serverResponse);
+            }
 
-        /* **Catch any IOException that may occur and print the stack trace. 
+        }
+         /* **Catch any IOException that may occur and print the stack trace.
             An IOException is thrown when an input-output operation is failed or interrupted.** */
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- Year 3/Client Server/Week_03_TUT/Tutorial-week03-socket2/src/main/java/com/mycompany/tutorial/week03/socket2/SimpleChatServer.java
+++ Year 3/Client Server/Week_03_TUT/Tutorial-week03-socket2/src/main/java/com/mycompany/tutorial/week03/socket2/SimpleChatServer.java
@@ -7,33 +7,56 @@ package com.mycompany.tutorial.week03.socket2;
 //import necessary libraries
 
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class SimpleChatServer {
     public static void main(String[] args) {
         try {
             //Create a new instance of the Server Socket and pass port number
-            
+            ServerSocket serverSocket = new ServerSocket(12345);
+
             //print out a message to say Server is running 
+            System.out.println("Server is Running");
 
             // Wait for a client to connect and accept the client request
+            Socket clientSocket = serverSocket.accept();
 
             //print out a message to say client connected and get the IP ddress
+            System.out.println("Client Connected: " + clientSocket.getInetAddress().getHostAddress());
 
             // Input stream to receive messages from the client
+            InputStream in = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
             // Output stream to send messages to the client
+            OutputStream out = clientSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(out,true);
 
-            
             //Create a buffer array with type byte, the size must be 1024
-           
+            byte[] bufferArr = new byte[1024];
 
             // Read messages from the client and print them
-            
-            
+            Scanner inp = new Scanner(System.in);
+            String message;
+            while((message = reader.readLine()) != null){
+                System.out.println("Client: " + message);
+
+                System.out.println("Enter message: ");
+                String msg = inp.nextLine();
+                writer.println(msg);
+            }
 
             // Close the sockets
-           
-            
-        //catch IO exception
-        
+            clientSocket.close();
+            serverSocket.close();
+
+            //catch IO exception
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
